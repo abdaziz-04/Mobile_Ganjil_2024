0a881e4bb1de2abb60736528096646d65413fc0b
abdaziz04 <abdaziz1713@gmail.com>
2024-09-19 09:31:26 +0700

    praktikum 3 - Langkah 3

--- PengantarDartBag3/Praktikum3.dart
+++ PengantarDartBag3/Praktikum3.dart
@@ -11,6 +11,19 @@ void main() {
     10: 'neon',
   };
 
+  var mhs1 = Map<String, String>();
+  gifts['first'] = 'patridge';
+  gifts['second'] = 'turtledoves';
+  gifts['fifth'] = 'golden rings';
+
+  var mhs2 = Map<int, String>();
+  nobleGases[2] = 'helium';
+  nobleGases[10] = 'neon';
+  nobleGases[18] = 'argon';
+
+  print("=========================");
   print(gifts);
   print(nobleGases);
+  print(mhs1);
+  print(mhs2);
 }
